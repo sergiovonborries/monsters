4a5806f6b0ccc586ea3e06d1f4d206a7684186b9
Sergio von Borries <svonborries@gmail.com>
2019-07-13 10:48:11 -0400

    Updates

---
+++ precache-manifest.6136e088c343b1b3ce6fddef84bfe481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3c846bcad5d90d6bb6928f4bad8ec4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "23a1b91ee1cc7471bb31",
+    "url": "/monsters/static/css/main.c6c9a931.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "23a1b91ee1cc7471bb31",
+    "url": "/monsters/static/js/main.67be2501.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
